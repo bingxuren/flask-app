3ce04d037cddd4d23c45ce69fd1cbb0c51339398
Bingxu <bingxu@core10.io>
2018-07-04 11:23:23 -0500

    add query schema

--- schema.py
+++ schema.py
@@ -1,12 +1,55 @@
 import graphene
 
-class Query(graphene.ObjectType):
-    greeting = graphene.String(name=graphene.String(default_value='stranger'))
+from .data import get_developer, get_employee, get_manager, get_teammates
 
-    def say_hello(self, info, name):
-        return 'hello %s' % name
+
+class Team(graphene.Enum):
+    UI = 0
+    FRONTEND = 1
+    BACKEND = 2
+    MANAGEMENT = 3
+
+
+# noinspection PyTypeChecker
+class Employee(graphene.Interface):
+    id = graphene.ID()
+    name = graphene.String()
+    teammates = graphene.List(lambda: Employee)
+    team_of = graphene.List(Team)
+
+    def resolve_teammates(self, info):
+        return [get_teammates(t) for t in self.teammates]
+
+
+class Developer(graphene.ObjectType):
+
+    class Meta:
+        interfaces = (Employee, )
+
+    gender = graphene.String()
+
+
+class Manager(graphene.ObjectType):
+
+    class Meta:
+        interfaces = (Employee, )
+    role = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    employee = graphene.Field(Employee, id=graphene.String())
+    developer = graphene.Field(Developer, id=graphene.String())
+    manager = graphene.Field(Manager, id=graphene.String())
+
+    def resolve_employee(info, id):
+        return 'emplyee'
+        # return get_employee(id)
+
+    def resolve_developer(info, id):
+        return get_developer(id)
+
+    def resolve_manager(info, id):
+        return get_manager(id)
+
 
 schema = graphene.Schema(query=Query)
-
-result = schema.execute('{ hello }')
-print(result.data['hello'])
